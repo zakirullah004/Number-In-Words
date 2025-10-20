6eb47ff2adb69636910f48640d65288709c991f9
zakirullah004 <zk743087@gmail.com>
2025-10-20 17:45:22 +0500

    0-999 completed

---
+++ app.js
@@ -0,0 +1,59 @@
+let allNumbers = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety"
+};
+
+function convertNumbertoWord(num){
+    if(allNumbers[num]) return allNumbers[num]
+
+    let word = "";
+
+    if(num >= 100){
+        word += convertNumbertoWord(Math.floor(num / 100)) + " Hundred ";
+        num %= 100;
+    }
+
+    if(num > 20){
+        if(word != "") word += 'and ';
+
+        word += convertNumbertoWord(Math.floor(num / 10)*10);
+        num %= 10;
+
+        if(num % 10 > 0){
+            word += " " +allNumbers[num%10]
+        }
+
+    }else if(num%100 < 20){
+       word += allNumbers[num]
+    }
+
+    return word
+
+}
+console.log(convertNumbertoWord(398));
